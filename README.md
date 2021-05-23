326324153f8f7ccb71dc01e4ef137cfa665ccbd8
KangWon LEE <kangwon.lee.edu@gmail.com>
2021-05-23 17:08:31 +0000

    break -> continue

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# python-break
-python break practice
+# python-continue
+python continue practice
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 for i in (1, 2, 3, 4,):
+  if 2 < i:
+    continue
   print("i =", i)
 
 print("i after the for loop =", i)
