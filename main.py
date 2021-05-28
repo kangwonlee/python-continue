66e570b6391a142a13aca16f27f242e528ad5896
KangWon LEE <kangwon.lee.edu@gmail.com>
2021-05-28 05:47:43 +0000

    two for loops

--- main.py
+++ main.py
@@ -2,8 +2,14 @@
 
 for i in (1, 2, 3, 4,):
   print("\ni =", end=' ')
-  if 2 < i:
-    continue
   print(i, end='')
 
 print("\ni after the for loop =", i)
+
+for j in (1, 2, 3, 4,):
+  print("\nj =", end=' ')
+  if 2 < j:
+    continue
+  print(j, end='')
+
+print("\nj after the for loop =", j)
